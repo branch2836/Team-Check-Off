6d0a253388c99103645373f3f0b0d92f219bed78
Branch, Christopher <Christopher_Branch@comcast.com>
2023-03-09 12:52:23 -0500

    first commmit

---
+++ data.js
@@ -0,0 +1,6 @@
+let teamList = ['Abishek', 'Abinandhan', 'Aditya', 'Annie', 'Azhagan', 'Balaji', 'Geetika',
+'Gowthami', 'Jagadesh', 'Jaya Prakash', 'Jayasudha', 'Leeladhar', 'Menaka',
+'Mohan', 'Mohini', 'Naveen', '-Nitin', 'Pawan', 'Prajakta', 'Pritikant',
+'Raj', 'Rajesh', 'Ram', 'Reddy', 'Riddhi', 'Revathy', 'Rucika', 'Sai', 'Saravana', 'Satta',
+'-Scott', 'Srikanth', 'Supriya', 'Swati', 'Tai', 'Tanusree', '-Vaikunth', 'Vijay',
+'Vikash', 'Viswa', 'Michael', 'Chris'];
